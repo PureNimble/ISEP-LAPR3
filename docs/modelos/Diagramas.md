b23cc4908f279c225bf0efed6658e2ec6adc29c6
João Pinto <1221694@isep.ipp.pt>
2023-10-22 17:51:12 +0100

    ISSUE-3 ISSUE-2 #time 10m #Resolução-em-análise
    - Transformação de primaryKeys artificiais para primaryKeys naturais
    - Retificação das tabelas do DDL.sql

--- docs/modelos/Diagramas.md
+++ docs/modelos/Diagramas.md
@@ -3,13 +3,13 @@
 
 ## Modelo Conceptual
 
-![ModeloConceptual](modelos/svg/ModeloConceptual.svg)
+![ModeloConceptual](svg/ModeloConceptual.svg)
 
 ## Modelo Lógico
 
-![ModeloLogico](modelos/svg/ModeloLógico.svg)
+![ModeloLogico](svg/ModeloLógico.svg)
 
 
 ## Modelo Físico
-![ModeloFisico](modelos/svg/ModeloFísico.svg)
+![ModeloFisico](svg/ModeloFísico.svg)
 
--- src/main/sql/DDL.sql
+++ src/main/sql/DDL.sql
@@ -30,45 +30,44 @@ CREATE TABLE CadernoCampo (
   ID number(10) GENERATED AS IDENTITY, 
   PRIMARY KEY (ID));
 CREATE TABLE Cultura (
-  ID            number(10) GENERATED AS IDENTITY, 
-  Especie       varchar2(255) NOT NULL, 
-  NomeComum     varchar2(255) NOT NULL, 
-  Variedade     varchar2(255) NOT NULL, 
-  Poda          varchar2(255) NOT NULL, 
-  Floracao      varchar2(255) NOT NULL, 
-  Colheita      varchar2(255) NOT NULL, 
-  Sementeira    varchar2(255) NOT NULL, 
-  TipoCulturaID number(10) NOT NULL, 
+  ID                    number(10) GENERATED AS IDENTITY, 
+  Especie               varchar2(255) NOT NULL, 
+  NomeComum             varchar2(255) NOT NULL, 
+  Variedade             varchar2(255) NOT NULL, 
+  Poda                  varchar2(255), 
+  Floracao              varchar2(255), 
+  Colheita              varchar2(255), 
+  Sementeira            varchar2(255), 
+  TipoCulturaDesignacao varchar2(255) NOT NULL, 
   PRIMARY KEY (ID));
 CREATE TABLE Edificio (
-  ID             number(10) GENERATED AS IDENTITY, 
-  Designacao     varchar2(255) NOT NULL, 
-  Area           number(10) NOT NULL, 
-  Unidade        varchar2(255) NOT NULL, 
-  QuintaID       number(10) NOT NULL, 
-  TipoEdificioID number(10) NOT NULL, 
+  ID                     number(10) GENERATED AS IDENTITY, 
+  Designacao             varchar2(255) NOT NULL, 
+  Area                   number(10), 
+  Unidade                varchar2(255), 
+  QuintaID               number(10) NOT NULL, 
+  TipoEdificioDesignacao varchar2(255) NOT NULL, 
   PRIMARY KEY (ID));
 CREATE TABLE Elemento (
-  ID         number(10) GENERATED AS IDENTITY, 
   Designacao varchar2(255) NOT NULL, 
-  PRIMARY KEY (ID));
+  PRIMARY KEY (Designacao));
 CREATE TABLE ElementoFicha (
-  FichaTecnicaID number(10) NOT NULL, 
-  ElementoID     number(10) NOT NULL, 
-  Quantidade     number(10) NOT NULL, 
-  Unidade        varchar2(255) NOT NULL, 
+  FichaTecnicaID     number(10) NOT NULL, 
+  ElementoDesignacao varchar2(255) NOT NULL, 
+  Quantidade         number(10) NOT NULL, 
+  Unidade            varchar2(255) NOT NULL, 
   PRIMARY KEY (FichaTecnicaID, 
-  ElementoID));
+  ElementoDesignacao));
 CREATE TABLE EstacaoMeteorologica (
   ID       number(10) GENERATED AS IDENTITY, 
   QuintaID number(10) NOT NULL, 
   PRIMARY KEY (ID));
 CREATE TABLE FatorDeProducao (
-  ID            number(10) GENERATED AS IDENTITY, 
-  Designacao    varchar2(255) NOT NULL, 
-  Fabricante    varchar2(255) NOT NULL, 
-  Formato       varchar2(255) NOT NULL, 
-  TipoProdutoID number(10) NOT NULL, 
+  ID                    number(10) GENERATED AS IDENTITY, 
+  Designacao            varchar2(255) NOT NULL, 
+  Fabricante            varchar2(255) NOT NULL, 
+  Formato               varchar2(255) NOT NULL, 
+  TipoProdutoDesignacao varchar2(255) NOT NULL, 
   PRIMARY KEY (ID));
 CREATE TABLE Fertilizacao (
   OperacaoID         number(10) NOT NULL, 
@@ -84,13 +83,13 @@ CREATE TABLE ModoFertilizicao (
   Designacao varchar2(255) NOT NULL, 
   PRIMARY KEY (ID));
 CREATE TABLE Operacao (
-  ID             number(10) GENERATED AS IDENTITY, 
-  Data           date NOT NULL, 
-  Quantidade     number(10) NOT NULL, 
-  Unidade        varchar2(255) NOT NULL, 
-  PlantacaoID    number(10) NOT NULL, 
-  TipoOperacaoID number(10) NOT NULL, 
-  CadernoCampoID number(10) NOT NULL, 
+  ID                     number(10) GENERATED AS IDENTITY, 
+  Data                   date NOT NULL, 
+  Quantidade             number(10), 
+  Unidade                varchar2(255), 
+  PlantacaoID            number(10) NOT NULL, 
+  CadernoCampoID         number(10) NOT NULL, 
+  TipoOperacaoDesignacao varchar2(255) NOT NULL, 
   PRIMARY KEY (ID));
 CREATE TABLE Parcela (
   ID         number(10) GENERATED AS IDENTITY, 
@@ -99,16 +98,17 @@ CREATE TABLE Parcela (
   QuintaID   number(10) NOT NULL, 
   PRIMARY KEY (ID));
 CREATE TABLE Plantacao (
-  ID          number(10) GENERATED AS IDENTITY, 
-  DataInicial date NOT NULL, 
-  DataFinal   date NOT NULL, 
-  Quantidade  number(10) NOT NULL, 
-  Unidade     varchar2(255) NOT NULL, 
-  ParcelaID   number(10) NOT NULL, 
-  CulturaID   number(10) NOT NULL, 
+  ID              number(10) GENERATED AS IDENTITY, 
+  DataInicial     date NOT NULL, 
+  DataFinal       date, 
+  Quantidade      number(10) NOT NULL, 
+  Unidade         varchar2(255) NOT NULL, 
+  ParcelaID       number(10) NOT NULL, 
+  CulturaID       number(10) NOT NULL, 
+  CulturaFloracao varchar2(255) NOT NULL, 
   PRIMARY KEY (ID));
 CREATE TABLE Quinta (
-  ID         number(10) GENERATED AS IDENTITY, 
+  ID         number(10) NOT NULL, 
   Designacao varchar2(255) NOT NULL, 
   PRIMARY KEY (ID));
 CREATE TABLE RegistoSensor (
@@ -124,46 +124,41 @@ CREATE TABLE Sensor (
   TipoSensorID number(10) NOT NULL, 
   PRIMARY KEY (ID));
 CREATE TABLE TipoCultura (
-  ID         number(10) GENERATED AS IDENTITY, 
   Designacao varchar2(255) NOT NULL, 
-  PRIMARY KEY (ID));
+  PRIMARY KEY (Designacao));
 CREATE TABLE TipoEdificio (
-  ID         number(10) GENERATED AS IDENTITY, 
   Designacao varchar2(255) NOT NULL, 
-  PRIMARY KEY (ID));
+  PRIMARY KEY (Designacao));
 CREATE TABLE TipoOperacao (
-  ID         number(10) GENERATED AS IDENTITY, 
   Designacao varchar2(255) NOT NULL, 
-  PRIMARY KEY (ID));
+  PRIMARY KEY (Designacao));
 CREATE TABLE TipoProduto (
-  ID         number(10) GENERATED AS IDENTITY, 
   Designacao varchar2(255) NOT NULL, 
-  PRIMARY KEY (ID));
+  PRIMARY KEY (Designacao));
 CREATE TABLE TipoSensor (
   ID         number(10) GENERATED AS IDENTITY, 
   Designacao varchar2(255) NOT NULL, 
   Unidade    varchar2(255) NOT NULL, 
   PRIMARY KEY (ID));
-  
-ALTER TABLE Edificio ADD CONSTRAINT FKEdificio778486 FOREIGN KEY (TipoEdificioID) REFERENCES TipoEdificio (ID);
-ALTER TABLE Edificio ADD CONSTRAINT FKEdificio681139 FOREIGN KEY (QuintaID) REFERENCES Quinta (ID);
-ALTER TABLE Parcela ADD CONSTRAINT FKParcela842886 FOREIGN KEY (QuintaID) REFERENCES Quinta (ID);
+ALTER TABLE Edificio ADD CONSTRAINT FKEdificio212023 FOREIGN KEY (TipoEdificioDesignacao) REFERENCES TipoEdificio (Designacao);
 ALTER TABLE Plantacao ADD CONSTRAINT FKPlantacao85856 FOREIGN KEY (ParcelaID) REFERENCES Parcela (ID);
 ALTER TABLE Operacao ADD CONSTRAINT FKOperacao529665 FOREIGN KEY (PlantacaoID) REFERENCES Plantacao (ID);
 ALTER TABLE AplicaoProduto ADD CONSTRAINT FKAplicaoPro761794 FOREIGN KEY (FatorDeProducaoID) REFERENCES FatorDeProducao (ID);
 ALTER TABLE FichaTecnica ADD CONSTRAINT FKFichaTecni776683 FOREIGN KEY (FatorDeProducaoID) REFERENCES FatorDeProducao (ID);
 ALTER TABLE ElementoFicha ADD CONSTRAINT FKElementoFi602363 FOREIGN KEY (FichaTecnicaID) REFERENCES FichaTecnica (ID);
 ALTER TABLE RegistoSensor ADD CONSTRAINT FKRegistoSen577174 FOREIGN KEY (EstacaoMeteorologicaID) REFERENCES EstacaoMeteorologica (ID);
-ALTER TABLE Operacao ADD CONSTRAINT FKOperacao623592 FOREIGN KEY (TipoOperacaoID) REFERENCES TipoOperacao (ID);
-ALTER TABLE FatorDeProducao ADD CONSTRAINT FKFatorDePro879583 FOREIGN KEY (TipoProdutoID) REFERENCES TipoProduto (ID);
-ALTER TABLE EstacaoMeteorologica ADD CONSTRAINT FKEstacaoMet933281 FOREIGN KEY (QuintaID) REFERENCES Quinta (ID);
+ALTER TABLE Operacao ADD CONSTRAINT FKOperacao50794 FOREIGN KEY (TipoOperacaoDesignacao) REFERENCES TipoOperacao (Designacao);
+ALTER TABLE FatorDeProducao ADD CONSTRAINT FKFatorDePro533831 FOREIGN KEY (TipoProdutoDesignacao) REFERENCES TipoProduto (Designacao);
 ALTER TABLE RegistoSensor ADD CONSTRAINT FKRegistoSen307126 FOREIGN KEY (SensorID) REFERENCES Sensor (ID);
 ALTER TABLE Sensor ADD CONSTRAINT FKSensor879684 FOREIGN KEY (TipoSensorID) REFERENCES TipoSensor (ID);
 ALTER TABLE RegistoSensor ADD CONSTRAINT FKRegistoSen457242 FOREIGN KEY (CadernoCampoID) REFERENCES CadernoCampo (ID);
 ALTER TABLE Operacao ADD CONSTRAINT FKOperacao517013 FOREIGN KEY (CadernoCampoID) REFERENCES CadernoCampo (ID);
 ALTER TABLE Plantacao ADD CONSTRAINT FKPlantacao171838 FOREIGN KEY (CulturaID) REFERENCES Cultura (ID);
-ALTER TABLE Cultura ADD CONSTRAINT FKCultura303785 FOREIGN KEY (TipoCulturaID) REFERENCES TipoCultura (ID);
+ALTER TABLE Cultura ADD CONSTRAINT FKCultura163980 FOREIGN KEY (TipoCulturaDesignacao) REFERENCES TipoCultura (Designacao);
 ALTER TABLE Fertilizacao ADD CONSTRAINT FKFertilizac483896 FOREIGN KEY (OperacaoID) REFERENCES Operacao (ID);
 ALTER TABLE Fertilizacao ADD CONSTRAINT FKFertilizac457773 FOREIGN KEY (ModoFertilizicaoID) REFERENCES ModoFertilizicao (ID);
 ALTER TABLE Fertilizacao ADD CONSTRAINT FKFertilizac478523 FOREIGN KEY (FatorDeProducaoID) REFERENCES FatorDeProducao (ID);
-ALTER TABLE ElementoFicha ADD CONSTRAINT FKElementoFi523793 FOREIGN KEY (ElementoID) REFERENCES Elemento (ID);
+ALTER TABLE ElementoFicha ADD CONSTRAINT FKElementoFi867111 FOREIGN KEY (ElementoDesignacao) REFERENCES Elemento (Designacao);
+ALTER TABLE Edificio ADD CONSTRAINT FKEdificio681139 FOREIGN KEY (QuintaID) REFERENCES Quinta (ID);
+ALTER TABLE Parcela ADD CONSTRAINT FKParcela842886 FOREIGN KEY (QuintaID) REFERENCES Quinta (ID);
+ALTER TABLE EstacaoMeteorologica ADD CONSTRAINT FKEstacaoMet933281 FOREIGN KEY (QuintaID) REFERENCES Quinta (ID);
