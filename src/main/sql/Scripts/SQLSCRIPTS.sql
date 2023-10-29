da54e4706aa41d4644eef2c10e81f8ef396d7a4d
Rafael Araújo <1201804@isep.ipp.pt>
2023-10-29 05:24:14 +0000

    ISSUE-6 #time 10m
    - Adição de um script alternativo.

--- src/main/sql/Scripts/SQLSCRIPTS.sql
+++ src/main/sql/Scripts/SQLSCRIPTS.sql
@@ -1335,30 +1335,29 @@ IS
     v_ParcelaID NUMBER;
     v_ParcelaName VARCHAR2(100);
 BEGIN
-    -- Retrieve the Parcela ID and Name
     SELECT ID, Designacao
     INTO v_ParcelaID, v_ParcelaName
     FROM Parcela
     WHERE Designacao = p_Parcela;
 
-    DBMS_OUTPUT.PUT_LINE('Parcela ID: ' || v_ParcelaID);
-    DBMS_OUTPUT.PUT_LINE('Parcela Name: ' || v_ParcelaName);
+    DBMS_OUTPUT.PUT_LINE('ID da Parcela: ' || v_ParcelaID);
+    DBMS_OUTPUT.PUT_LINE('Nome da Parcela: ' || v_ParcelaName);
 
     FOR rec IN (
         SELECT TP.Designacao AS Tipo, COUNT(F.OperacaoID) AS numeroOperacoes
         FROM Operacao OP
-        INNER JOIN Fertilizacao F ON OP.ID = F.OperacaoID
-        INNER JOIN FatorDeProducao FP ON F.FatorDeProducaoID = FP.ID
-        INNER JOIN TipoProduto TP ON FP.TipoProdutoID = TP.ID
+        INNER JOIN Fertilizacao F ON OP.ID = F.OperacaoID             -- obter Fertilizações
+        INNER JOIN FatorDeProducao FP ON F.FatorDeProducaoID = FP.ID  -- obter Fatores de Produção
+        INNER JOIN TipoProduto TP ON FP.TipoProdutoID = TP.ID         -- obter Tipo de Produto
         WHERE OP.ParcelaID = v_ParcelaID
         AND OP.DataOperacao BETWEEN p_StartDate AND p_EndDate
         GROUP BY TP.Designacao
         UNION ALL
         SELECT TP.Designacao AS Tipo, COUNT(AF.OperacaoID) AS numeroOperacoes
         FROM Operacao OP
-        INNER JOIN AplicacaoFitofarmaco AF ON OP.ID = AF.OperacaoID
-        INNER JOIN FatorDeProducao FP ON AF.FatorDeProducaoID = FP.ID
-        INNER JOIN TipoProduto TP ON FP.TipoProdutoID = TP.ID
+        INNER JOIN AplicacaoFitofarmaco AF ON OP.ID = AF.OperacaoID   -- obter Aplicações Fitofarmacos
+        INNER JOIN FatorDeProducao FP ON AF.FatorDeProducaoID = FP.ID -- obter Fatores de Produção
+        INNER JOIN TipoProduto TP ON FP.TipoProdutoID = TP.ID         -- obter Tipo de Produto
         WHERE OP.ParcelaID = v_ParcelaID
         AND OP.DataOperacao BETWEEN p_StartDate AND p_EndDate
         GROUP BY TP.Designacao
@@ -1368,7 +1367,50 @@ BEGIN
 END;
 /
 
+CREATE OR REPLACE PROCEDURE getNumberOfFactorsByTypeV2(
+    p_Parcela VARCHAR2,
+    p_StartDate DATE,
+    p_EndDate DATE
+)
+IS
+    v_ParcelaID NUMBER;
+    v_ParcelaName VARCHAR2(100);
+BEGIN
+    -- Retrieve the Parcela ID and Name
+    SELECT ID, Designacao
+    INTO v_ParcelaID, v_ParcelaName
+    FROM Parcela
+    WHERE Designacao = p_Parcela;
+
+    DBMS_OUTPUT.PUT_LINE('ID da Parcela: ' || v_ParcelaID);
+    DBMS_OUTPUT.PUT_LINE('Nome da Parcela: ' || v_ParcelaName);
+
+    FOR rec IN (
+        SELECT TP.Designacao AS Tipo, COUNT(DISTINCT FP.Designacao) AS numeroOperacoes
+        FROM Operacao OP
+        INNER JOIN Fertilizacao F ON OP.ID = F.OperacaoID              -- obter Fertilizações
+        INNER JOIN FatorDeProducao FP ON F.FatorDeProducaoID = FP.ID   -- obter Fatores de Produção
+        INNER JOIN TipoProduto TP ON FP.TipoProdutoID = TP.ID          -- obter Tipo de Produto
+        WHERE OP.ParcelaID = v_ParcelaID
+        AND OP.DataOperacao BETWEEN p_StartDate AND p_EndDate
+        GROUP BY TP.Designacao
+        UNION ALL
+        SELECT TP.Designacao AS Tipo, COUNT(DISTINCT FP.Designacao) AS numeroOperacoes
+        FROM Operacao OP
+        INNER JOIN AplicacaoFitofarmaco AF ON OP.ID = AF.OperacaoID    -- obter Aplicações Fitofarmacos
+        INNER JOIN FatorDeProducao FP ON AF.FatorDeProducaoID = FP.ID  -- obter Fatores de Produção
+        INNER JOIN TipoProduto TP ON FP.TipoProdutoID = TP.ID          -- obter Tipo de Produto
+        WHERE OP.ParcelaID = v_ParcelaID
+        AND OP.DataOperacao BETWEEN p_StartDate AND p_EndDate
+        GROUP BY TP.Designacao
+    ) LOOP
+        DBMS_OUTPUT.PUT_LINE('Tipo de Fator De Produção: ' || rec.Tipo || '; Número de Fatores de Produção: ' || rec.numeroOperacoes);
+    END LOOP;
+END;
+/
+
 BEGIN 
     getNumberOfFactorsByType('Lameiro da ponte', TO_DATE('2003-01-01', 'yyyy-mm-dd'), TO_DATE('2022-12-31', 'yyyy-mm-dd')); 
+    getNumberOfFactorsByTypeV2('Lameiro da ponte', TO_DATE('2003-01-01', 'yyyy-mm-dd'), TO_DATE('2022-12-31', 'yyyy-mm-dd')); 
 END; 
 /
