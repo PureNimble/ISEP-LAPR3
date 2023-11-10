efd05fecafebf9e4df349e6372b7a849a662449a
Rafael Araújo <1201804@isep.ipp.pt>
2023-11-10 15:49:03 +0000

    ISSUE-80 #time 30m #Resolução-em-análise
    - Alteração do ficheiro .s devido às alterações do enunciado

--- ARQCP/ex3/asm.s
+++ ARQCP/ex3/asm.s
@@ -3,21 +3,38 @@
 	.global copy_num_vec
 
 copy_num_vec:
-    movl $0, %eax
+    #rdi -> array
+    #rsi -> length
+    #rdx -> &read
+    #rcx -> &write
+    #r8 -> num
+    #r9 -> vec
 
-    cmpl %ecx, %edx
-    jl end
+    movl (%rdx), %r10d            # get read index
+    movl (%rcx), %eax             # get write index
+
+    subl %r10d, %eax              # writeIndex -= readIndex
+    addl %esi, %eax               # writeIndex += length
+    xorl %edx, %edx               # clear edx
+    divl %esi                     # calculate writeIndex % length
+
+    xorl %eax, %eax               # clear return value
+    incl %edx                     # increment remainder
+    cmpl %edx, %r8d
+    jg end                        # jump to end if (initialized elements < num)
+
+    movl %r8d, %ecx               # counter for num elements
 
 array_to_vec:
-    movl (%rdi, %rsi, 4), %r10d
-    movl %r10d, (%r8)
-    addq $4, %r8
-    incl %esi
-    cmpl %esi, %edx
-    cmove %eax, %esi
-    loop array_to_vec
+    movl (%rdi, %r10, 4), %r11d   # get element of array[readIndex]
+    movl %r11d, (%r9)             # add element to vec
+    addq $4, %r9                  # next position of vec
+    incl %r10d                    # readIndex++
+    cmpl %r10d, %esi              # check end of array
+    cmove %eax, %r10d             # if (readIndex == length) readIndex = 0
+    loop array_to_vec             # go to next iteration
 
-    incl %eax
+    incl %eax                     # increment return value
 
 end:
-	ret
+	ret                           #return
--- ARQCP/ex3/main.c
+++ ARQCP/ex3/main.c
@@ -2,9 +2,10 @@
 #include "asm.h"
 
 int main() {
-  int num = 4, read = 0, write = 6;
+  int length = 8, num = 7, read = 4, write = 3;
   int array[] = {1, 2, 3, 4, 5, 6, 7, 8}, vec[num];
-  if (copy_num_vec(array, read, write, num, vec)) {
+  if (copy_num_vec(array, length, &read, &write, num, vec)) {
+    printf("%i elements were successfully copied to vec:\n", num);
     for (int i = 0; i < num; i++) {
       printf("vec[%i]: %i\n", i, vec[i]);
     }
