64b9181eb350bdceb0200360800b503b03a73691
José Sá <1220612@isep.ipp.pt>
2023-11-11 19:54:06 +0000

    ISSUE-78 #time 1h #Resolução-em-análise Implemantação da USAC01

--- ARQCP/ex1/asm.s
+++ ARQCP/ex1/asm.s
@@ -8,43 +8,54 @@
 	.global extract_token
 
 extract_token:
-    movl (%rdx), %ecx
-
-    pushq %rsi
+    pushq %rbx
 
 token_loop:
-    popq %rsi
-    movb (%rdi), %r8b
-    movb (%rsi), %r9b
-    cmpb %r8b, %r9b         #compare first char of token with al
-    je check_token
+    movb (%rdi), %bl
+    movb (%rsi), %cl
+    cmpb %bl, %cl           #compare first char of token with al
+    je check_token_prologue
     incq %rdi               # input[i++]
     jmp token_loop
+
+check_token_prologue:
+    pushq %rsi
 
 check_token:
     incq %rdi               # input[i++]
     incq %rsi               # token[i++]
-    movb (%rdi), %r8b
-    movb (%rsi), %r9b
-    cmpb %r8b, %r9b         # if input[i] != token[i]
-    jne token_loop
-    cmpb $58, %r8b          # if input[i] == ':'
-    je extract_value
+    movb (%rdi), %bl
+    movb (%rsi), %cl
+    cmpb $0, %cl           # if token reaches end
+    je extract_value_prologue
+    cmpb %bl, %cl           # if input[i] != token[i]
+    jne check_token_epilogue
     jmp check_token         # loop check_token
+
+check_token_epilogue:
+    popq %rsi
+    jmp token_loop
+
+extract_value_prologue:
+    popq %rsi
 
 extract_value:
     incq %rdi
-    movb (%rdi), %r8b
-    cmpb $46, %r8b
+    movb (%rdi), %bl
+    cmpb $46, %bl
     je end
-    cmpb $35, %r8b
+    cmpb $35, %bl
     je end
-    cmpb $0, %r8b
+    cmpb $0, %bl
     je end
+    movl (%rdx), %ecx
     imul $10, %ecx
-    movsbl %r8b, %eax
+    movsbl %bl, %eax
+    subl $48, %eax
     addl %eax, %ecx
+    movl %ecx, (%rdx)
     jmp extract_value
 
 end:
+    popq %rbx
 	ret
--- ARQCP/ex1/main.c
+++ ARQCP/ex1/main.c
@@ -3,7 +3,7 @@
 
 int main() {
   char input[] = "sensor_id:8#type:atmospheric_temperature#value:21.60#unit:celsius#time:2470030";
-  char token[] = "sensor_id";
+  char token[] = "time";
   int output = 0;
   extract_token(input, token, &output);
   printf("%d\n", output);
