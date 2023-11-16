d7bafc9b62cb9879b7c233acb3b93800c7df63c6
Rafael Araújo <1201804@isep.ipp.pt>
2023-11-16 19:38:25 +0000

    ISSUE-135 ISSUE-39 #time 30m
    - Criação de teste para a US EI01

--- src/test/java/isep/lapr3/g094/struct/MainTest.java
+++ src/test/java/isep/lapr3/g094/struct/MainTest.java
@@ -6,36 +6,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
+import isep.lapr3.g094.application.controller.ImportController;
 import isep.lapr3.g094.domain.Location;
-import isep.lapr3.g094.imports.Import;
 import isep.lapr3.g094.struct.graph.map.BasketDistribution;
 
 public class MainTest {
-    private Import imports;
     private List<String> distances;
     private List<Location> locations;
     private BasketDistribution basketDistribution;
 
     @BeforeEach
     void setUp() {
-        imports = new Import();
-        distances = imports.importDistances();
-        locations = imports.importLocations();
+        ImportController importController = new ImportController();
+        distances = importController.importDistances();
+        locations = importController.importLocations();
+
         basketDistribution = new BasketDistribution();
-    }
-
-    @Test
-    void testImportDistances() {
-        assertEquals(imports.importDistances().size(), 33);
-    }
-
-    @Test
-    void testImportLocations() {
-        assertEquals(imports.importLocations().size(), 17);
-    }
-
-    @Test
-    void testBasketDistribution() {
         for (Location location : locations) {
             basketDistribution.addLocation(location);
         }
@@ -45,9 +31,84 @@ public class MainTest {
             basketDistribution.addDistance(basketDistribution.locationById(line[0]),
                     basketDistribution.locationById(line[1]), Integer.parseInt(line[2]));
         }
+    }
 
+    @Test
+    void testImportLocations() {
+        assertEquals(323, locations.size());
+    }
+
+    @Test
+    void testImportDistances() {
+        assertEquals(783, distances.size());
+    }
+
+    @Test
+    void testBasketDistribution() {
         System.out.println("Número de Localizações(Vértices): " + basketDistribution.getNumLocations());
         System.out.println("Número de Distâncias(Arestas): " + basketDistribution.getNumDistances());
         System.out.println(basketDistribution.toString());
+    }
+
+    @Test
+    void testBasketDistributionLocationsNum() {
+        System.out.println("Testing BasketDistributionLocationsNum...");
+
+        assertEquals(323, basketDistribution.getNumLocations());
+    }
+
+    @Test
+    void testBasketDistributionDistancesNum() {
+        System.out.println("Testing BasketDistributionDistancesNum...");
+
+        assertEquals(1566, basketDistribution.getNumDistances());
+    }
+
+    @Test
+    void testBasketDistributionNullLocation() {
+        System.out.println("Testing BasketDistributionNullLocation...");
+
+        assertThrows(RuntimeException.class, () -> basketDistribution.addLocation(null));
+        assertThrows(RuntimeException.class, () -> basketDistribution.addDistance(null, null, 0));
+    }
+
+    @Test
+    void testBasketDistributionLocationByKey() {
+        System.out.println("Testing BasketDistributionLocationByKey...");
+
+        assertEquals("CT43", basketDistribution.locationByKey(0).getId());
+        assertEquals("CT268", basketDistribution.locationByKey(12).getId());
+        assertEquals("CT137", basketDistribution.locationByKey(232).getId());
+        assertEquals("CT159", basketDistribution.locationByKey(322).getId());
+        assertNull(basketDistribution.locationByKey(999));
+    }
+
+    @Test
+    void testBasketDistributionKeyByLocation() {
+        System.out.println("Testing BasketDistributionKeyLocation...");
+
+        assertEquals(0, basketDistribution.keyLocation(locations.stream()
+                .filter(l -> "CT43".equals(l.getId())).findFirst().orElse(null)));
+        assertEquals(12, basketDistribution.keyLocation(locations.stream()
+                .filter(l -> "CT268".equals(l.getId())).findFirst().orElse(null)));
+        assertEquals(232, basketDistribution.keyLocation(locations.stream()
+                .filter(l -> "CT137".equals(l.getId())).findFirst().orElse(null)));
+        assertEquals(322, basketDistribution.keyLocation(locations.stream()
+                .filter(l -> "CT159".equals(l.getId())).findFirst().orElse(null)));
+        assertEquals(-1, basketDistribution.keyLocation(null));
+    }
+
+    @Test
+    void testBasketDistributionLocationByID() {
+        System.out.println("Testing BasketDistributionLocationByID...");
+
+        Location location1 = new Location("CT123", 0, 0, 0);
+        Location location2 = new Location("CT268", 0, 0, 0);
+        basketDistribution.addLocation(location1);
+        basketDistribution.addLocation(location2);
+
+        assertEquals(location1, basketDistribution.locationById("CT123"));
+        assertEquals(location2, basketDistribution.locationById("CT268"));
+        assertNull(basketDistribution.locationById("CT999"));
     }
 }
