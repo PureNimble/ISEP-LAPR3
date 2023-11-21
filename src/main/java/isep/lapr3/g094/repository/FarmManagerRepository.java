42c47dc06434697bee80f4e34f71e4b412b16978
José Sá <1220612@isep.ipp.pt>
2023-11-21 17:28:00 +0000

    ISSUE-75 #time 30m Implementação da USLP06

--- src/main/java/isep/lapr3/g094/repository/FarmManagerRepository.java
+++ src/main/java/isep/lapr3/g094/repository/FarmManagerRepository.java
@@ -35,10 +35,23 @@ public class FarmManagerRepository {
 		return false;
 	}
 
-	public boolean registerColheita(int quantidade, int parcelaID, int plantacaoID, Date dataOperacao) {
-		// TODO Auto-generated method stub
-
-		return false;
+	public boolean registerColheita(int quantidade, int parcelaID, int plantacaoID, Date dataOperacao) throws SQLException{
+		CallableStatement callStmt = null;
+		try {
+			Connection connection = DatabaseConnection.getInstance().getConnection();
+			callStmt = connection.prepareCall("{ call registerColheita(?,?,?,?) }");
+			callStmt.setInt(1, plantacaoID);
+			callStmt.setInt(2, parcelaID);
+			callStmt.setDate(3, dataOperacao);
+			callStmt.setInt(4, quantidade);
+			return callStmt.execute();
+		} catch (SQLException e) {
+			throw e;
+		} finally {
+			if (callStmt != null) {
+				callStmt.close();
+			}
+		}
 	}
 
 	public boolean registerSemeadura(int quantidade, int parcelaID, int plantacaoID, Date dataOperacao) {
