8b42d380246e56c722a1eecec309ce90fdf8f6b8
José Sá <1220612@isep.ipp.pt>
2023-11-25 02:01:25 +0000

    ISSUE-65 #time 1h 30m #Resolução-em-análise Overhaul do script SQL

--- src/main/sql/Scripts/BD13/bd13.sql
+++ src/main/sql/Scripts/BD13/bd13.sql
@@ -6,18 +6,16 @@ CREATE OR REPLACE PROCEDURE registerColheita(plantacaoID NUMBER,parcelaID NUMBER
     idOperacao NUMBER;
 
 BEGIN
-    verifyColheitaInfo(plantacaoID,parcelaID,dataOperacao,quantidade,UNIDADE);
+    verifyParcelaInfo(parcelaID);
+    verifyPlantacaoInfo(plantacaoID, parcelaID, dataOperacao);
+    verifyDateInfo(dataOperacao);
     --Obter o ID da operação
     SELECT NVL(MAX(ID),0) + 1 INTO idOperacao FROM Operacao;
 
-    INSERT INTO Operacao(ID, DataOperacao, Quantidade, Unidade, TipoOperacaoID, CadernoCampoID) 
+    INSERT INTO Operacao(ID, DataOperacao, Quantidade, Unidade, TipoOperacaoID, CadernoCampoID)
     VALUES (idOperacao, dataOperacao, quantidade, UNIDADE, TIPO_OPERACAO, CADERNO_DE_CAMPO);
 
-    IF plantacaoID IS NULL THEN 
-        INSERT INTO OperacaoParcela(OperacaoID, ParcelaID) VALUES (idOperacao, parcelaID);
-    ELSE
-        INSERT INTO OperacaoPlantacao(OperacaoID, PlantacaoID) VALUES (idOperacao, plantacaoID);
-    END IF;
+    INSERT INTO OperacaoPlantacao(OperacaoID, PlantacaoID) VALUES (idOperacao, plantacaoID);
     COMMIT;
 EXCEPTION
     WHEN OTHERS THEN
