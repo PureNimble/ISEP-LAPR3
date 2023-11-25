9291321adbf5b5efa0bf7a6bd21ac96ec9ac6d5c
José Sá <1220612@isep.ipp.pt>
2023-11-25 23:34:02 +0000

    ISSUE-65 #time 30m Implementação do result cursor

--- src/main/sql/Scripts/SPRINT2/BD18/BD18.sql
+++ src/main/sql/Scripts/SPRINT2/BD18/BD18.sql
@@ -1,18 +1,45 @@
-CREATE OR REPLACE PROCEDURE GetOperacaoList(
-    p_StartDate DATE,
-    p_EndDate DATE,
-    parcelaID NUMBER(10)
-)
-IS
+CREATE OR REPLACE FUNCTION GetOperacaoList(parcelaID NUMBER,dataInicial DATE, dataFinal DATE) RETURN SYS_REFCURSOR AS
+    result_cursor SYS_REFCURSOR;
 BEGIN
-    SELECT OP.OperacaoID AS OperacaoID, TipoOperacao.Designacao AS TipoDeOperacao
-    FROM OperacaoParcela OP
-    WHERE OP.OperacaoID = Operacao.ID
-    AND Operacao.ID = TipoOperacao.ID
-    AND Operacao.DataOperacao BETWEEN p_StartDate AND p_EndDate
-    AND OP.ParcelaEspacoID = parcelaID
-    GROUP BY OP.Designacao
-    
+    OPEN result_cursor FOR
+    SELECT OperacaoID, TipoDeOperacao, data
+    FROM(
+        SELECT OP.OperacaoID AS OperacaoID, TOP.Designacao AS TipoDeOperacao, O.DataOperacao as data
+        FROM OperacaoParcela OP, TipoOperacao TOP, Operacao O
+        WHERE OP.OperacaoID = O.ID
+          AND O.ID = TOP.ID
+          AND OP.ParcelaEspacoID = parcelaID
+    )
+    WHERE data BETWEEN DATAINICIAL AND DATAFINAL
+    ORDER BY OperacaoID;
+RETURN result_cursor;
 END;
 /
 
+DECLARE
+result_cursor SYS_REFCURSOR;
+    operacaoid number(10);
+    tipo_de_operacao VARCHAR2(255);
+    data VARCHAR2(255);
+
+    data_found BOOLEAN := FALSE;
+BEGIN
+    result_cursor := GetOperacaoList(500, TO_DATE('2023-05-20', 'YYYY-MM-DD'), TO_DATE('2023-11-06', 'YYYY-MM-DD'));
+    LOOP
+        FETCH result_cursor INTO operacaoid, tipo_de_operacao, data;
+        EXIT WHEN result_cursor%NOTFOUND;
+        data_found := TRUE;
+        DBMS_OUTPUT.PUT_LINE(result_cursor%ROWCOUNT||' º'  );
+        DBMS_OUTPUT.PUT_LINE(' -> Operção: '|| operacaoid);
+        DBMS_OUTPUT.PUT_LINE(' -> Tipo de Operação:' || cultura );
+        DBMS_OUTPUT.PUT_LINE(' -> FatorProducao:'|| tipo_de_operacao);
+        DBMS_OUTPUT.PUT_LINE(' -> Data:'|| data);
+        DBMS_OUTPUT.PUT_LINE(' ');
+        DBMS_OUTPUT.PUT_LINE('---------------------------------');
+    END LOOP;
+CLOSE result_cursor;
+IF NOT data_found THEN
+        DBMS_OUTPUT.PUT_LINE('Sem resultados, para o intervalo de tempo selecionado!');
+END IF;
+END;
+/
