a6b7dbae0b6ff15cd8a5e29f7941610ba860316c
José Sá <1220612@isep.ipp.pt>
2023-11-26 10:32:32 +0000

    ISSUE-65 #time 30m #Resolução-Aceite Correção de erros

--- src/main/sql/Scripts/SPRINT2/BD18/BD18.sql
+++ src/main/sql/Scripts/SPRINT2/BD18/BD18.sql
@@ -1,17 +1,33 @@
 CREATE OR REPLACE FUNCTION GetOperacaoList(parcelaID NUMBER,dataInicial DATE, dataFinal DATE) RETURN SYS_REFCURSOR AS
     result_cursor SYS_REFCURSOR;
 BEGIN
-    OPEN result_cursor FOR
-    SELECT OperacaoID, TipoDeOperacao, data
-    FROM(
+OPEN result_cursor FOR
+SELECT OperacaoID, TipoDeOperacao, data
+FROM(
         SELECT OP.OperacaoID AS OperacaoID, TOP.Designacao AS TipoDeOperacao, O.DataOperacao as data
         FROM OperacaoParcela OP, TipoOperacao TOP, Operacao O
         WHERE OP.OperacaoID = O.ID
-          AND O.ID = TOP.ID
+          AND O.TIPOOPERACAOID = TOP.ID
           AND OP.ParcelaEspacoID = parcelaID
+        UNION
+        SELECT OP.OperacaoID AS OperacaoID, TOP.Designacao AS TipoDeOperacao, O.DataOperacao as data
+        FROM OperacaoPlantacao OP, TipoOperacao TOP, Operacao O, Plantacao P
+        WHERE OP.OperacaoID = O.ID
+          AND O.TIPOOPERACAOID = TOP.ID
+          AND OP.PlantacaoID = P.ID
+          AND P.ParcelaEspacoID = parcelaID
+        UNION
+        SELECT OS.OperacaoID AS OperacaoID, TOP.Designacao AS TipoDeOperacao, O.DataOperacao as data
+        FROM OperacaoSetor OS, TipoOperacao TOP, Operacao O, Plantacao P, PlantacaoSetor PS, Setor S
+        WHERE OS.OperacaoID = O.ID
+          AND O.TIPOOPERACAOID = TOP.ID
+          AND OS.SetorID = S.ID
+          AND P.PARCELAESPACOID = parcelaID
+          AND P.ID = PS.PlantacaoID
+          AND PS.SetorID = S.ID
     )
-    WHERE data BETWEEN DATAINICIAL AND DATAFINAL
-    ORDER BY OperacaoID;
+WHERE data BETWEEN DATAINICIAL AND DATAFINAL
+ORDER BY TipoDeOperacao, data;
 RETURN result_cursor;
 END;
 /
@@ -22,21 +38,25 @@ result_cursor SYS_REFCURSOR;
     tipo_de_operacao VARCHAR2(255);
     data VARCHAR2(255);
 
+    previous_tipoOperacao VARCHAR2(100) := NULL;
+
     data_found BOOLEAN := FALSE;
 BEGIN
-    result_cursor := GetOperacaoList(500, TO_DATE('2023-05-20', 'YYYY-MM-DD'), TO_DATE('2023-11-06', 'YYYY-MM-DD'));
+    result_cursor := GetOperacaoList(108, TO_DATE('2023-07-01', 'YYYY-MM-DD'), TO_DATE('2023-10-02', 'YYYY-MM-DD'));
     LOOP
-        FETCH result_cursor INTO operacaoid, tipo_de_operacao, data;
+FETCH result_cursor INTO operacaoid, tipo_de_operacao, data;
         EXIT WHEN result_cursor%NOTFOUND;
         data_found := TRUE;
-        DBMS_OUTPUT.PUT_LINE(result_cursor%ROWCOUNT||' º'  );
-        DBMS_OUTPUT.PUT_LINE(' -> Operção: '|| operacaoid);
-        DBMS_OUTPUT.PUT_LINE(' -> Tipo de Operação:' || cultura );
-        DBMS_OUTPUT.PUT_LINE(' -> FatorProducao:'|| tipo_de_operacao);
-        DBMS_OUTPUT.PUT_LINE(' -> Data:'|| data);
+        IF previous_tipoOperacao IS NULL OR previous_tipoOperacao <> tipo_de_operacao THEN
+            DBMS_OUTPUT.PUT_LINE('---------------------------------');
+            DBMS_OUTPUT.PUT_LINE('Tipo de Operação: ' || tipo_de_operacao);
+            DBMS_OUTPUT.PUT_LINE('---------------------------------');
+            previous_tipoOperacao := tipo_de_operacao;
+END IF;
+        DBMS_OUTPUT.PUT_LINE('-> Operação: ' || operacaoid);
+        DBMS_OUTPUT.PUT_LINE(CHR(9) || '-> Data: '|| data);
         DBMS_OUTPUT.PUT_LINE(' ');
-        DBMS_OUTPUT.PUT_LINE('---------------------------------');
-    END LOOP;
+END LOOP;
 CLOSE result_cursor;
 IF NOT data_found THEN
         DBMS_OUTPUT.PUT_LINE('Sem resultados, para o intervalo de tempo selecionado!');
