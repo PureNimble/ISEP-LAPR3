d2d23b0ad226f2a9340f01a24b2db02140821d11
Vasco Sousa <1221700@isep.ipp.pt>
2023-12-27 22:35:23 +0000

    ISSUE-17 ISSUE-163 #time 1h #Resolução-em-análise
    -Implementação da USBD30

--- src/main/sql/Scripts/SPRINT3/BD30/bd30.sql
+++ src/main/sql/Scripts/SPRINT3/BD30/bd30.sql
@@ -3,7 +3,14 @@ BEFORE UPDATE ON Operacao
 FOR EACH ROW
 DECLARE
     dataAtual DATE;
+    plantId NUMBER;
+    operationId NUMBER;
 BEGIN
+    SELECT PlantacaoID INTO plantId FROM OperacaoPlantacao WHERE OperacaoID = :OLD.ID;
+    SELECT COUNT(Op.OperacaoID) INTO operationId FROM OperacaoPlantacao Op, Operacao O WHERE Op.PlantacaoID = plantId AND Op.OperacaoID = O.ID AND O.DataOperacao > :OLD.DataOperacao;
+    IF operationId IS NOT NULL THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Não é possível alterar uma operação que tenha operações posteriores');
+    END IF;
     IF :NEW.ESTADOID = 3 THEN
         SELECT CURRENT_DATE INTO dataAtual FROM DUAL;
         IF (dataAtual - :OLD.DataOperacao) > 3 THEN
